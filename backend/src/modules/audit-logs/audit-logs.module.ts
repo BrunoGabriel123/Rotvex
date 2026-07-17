6eebd9869d1773a8ce5b4d5f7f9974aca9b8dd21
Bruno Gabriel <bg1821097@gmail.com>
2026-07-16 23:12:47 -0300

    corrige dependência do PrismaService no AuditLogsModule

--- backend/src/modules/audit-logs/audit-logs.module.ts
+++ backend/src/modules/audit-logs/audit-logs.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuditLogsService } from './audit-logs.service';
 import { AuditLogsInterceptor } from './audit-logs.interceptor';
 import { AuditLogsController } from './audit-logs.controller';
+import { PrismaService } from '../../common/database/prisma.service';
 
 @Module({
   controllers: [AuditLogsController],
-  providers: [AuditLogsService, AuditLogsInterceptor],
+  providers: [AuditLogsService, AuditLogsInterceptor, PrismaService],
   exports: [AuditLogsService, AuditLogsInterceptor],
 })
 export class AuditLogsModule {}
